6611939aac8d25d0936b41d2e15a44c71b0eac34
Dery <derykatze@hotmail.com>
2021-02-10 16:45:34 -0300

    TeamScore

---
+++ teammedia.js
@@ -0,0 +1,14 @@
+const calc = (a, b, c) =>
+    (a + b + c) / 3
+const koala = calc(44,23,71);
+const dolphi = calc(47,54,49);
+console.log (koala,dolphi)
+ const checkwinner = function (Dolphis, koalas) {
+     if (Dolphis > koalas){
+         console.log('Dolphis wins score: ' + dolphi)
+     }
+     else console.log('Koalas wins score: ' + koala)
+     
+ }
+checkwinner(dolphi,koala);
+  
